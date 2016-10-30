b0a5742897ae395aad9d9a6cf6fddbc7338585b0
Florian Marcu <florian.mrc@gmail.com>
2016-10-30 14:50:17 -0700

    Autolayout constraints for landing VC

--- Hickery/Hickery/LandingViewController.swift
+++ Hickery/Hickery/LandingViewController.swift
@@ -6,8 +6,16 @@
 //  Copyright © 2016 Florian Marcu. All rights reserved.
 //
 
+import FacebookLogin
 import UIKit
 
 class LandingViewController: UIViewController {
 
+    @IBOutlet var fbLoginContainerView: UIView!
+
+    override func viewDidLoad() {
+        let loginButton = LoginButton(readPermissions: [ .publicProfile ])
+
+        fbLoginContainerView.addSubview(loginButton)
+    }
 }
