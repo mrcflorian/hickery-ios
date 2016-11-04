a6548f1009dccfc26d66b3dc107a9025352a98cf
Florian Marcu <florian.mrc@gmail.com>
2016-11-03 22:00:36 -0700

    Integrated Crashlytics and Fabric

--- Hickery/Hickery/AppDelegate.swift
+++ Hickery/Hickery/AppDelegate.swift
@@ -10,6 +10,8 @@ import UIKit
 import FacebookCore
 import FacebookLogin
 import FacebookShare
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        Fabric.with([Crashlytics.self])
         return SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 
